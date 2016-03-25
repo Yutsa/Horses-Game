11b697c59b2af2bafdd39f82a65d215f3ab0320e
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2016-03-25 13:46:34 +0100

    Creation of the project, testing git integration in Eclipse.

---
+++ src/piece/PieceInterface.java
@@ -0,0 +1,16 @@
+/**
+ * This is the interface for the piece
+ */
+package piece;
+
+/**
+ * @author Édouard WILLISSECK
+ *
+ */
+public interface PieceInterface {
+	/**
+	 * Gives the x-axis position of the piece.
+	 * @return An integer corresponding to the x-axis position of the piece.
+	 */
+	int getPosX();
+}
