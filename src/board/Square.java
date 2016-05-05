3a920499c4c9fe364e1fc39b421a0fc704dab589
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2016-05-05 19:03:47 +0200

    Implemented the Square abstract class.

--- src/board/Square.java
+++ src/board/Square.java
@@ -1,32 +1,133 @@
 package board;
 
+import piece.Piece;
 import team.Team;
 
 public abstract class Square {
 	private int posX;
 	private int poxY;
 	private Team team;
+	private Piece pieceOnSquare;
+	private Board board;
+
+	//TODO: Finish the javadoc.
 	
-	//TODO: Make tests inside setters.
+	/**
+	 * Get the board the Square is linked to.
+	 * 
+	 * @return The Board the Square is linked to.
+	 */
+	public Board getBoard() {
+		return board;
+	}
+	
+	/**
+	 * Set the board the Square is linked to.
+	 * 
+	 * @param board The Board the Square is linked to.
+	 */
+	public void setBoard(Board board) {
+		if (board == null)
+			throw new IllegalArgumentException();
+		this.board = board;
+	}
+	
+	/**
+	 * Set a Piece on the Square.
+	 * 
+	 * @param piece The Piece to put on the Square.
+	 */
+	public void setPieceOnSquare(Piece piece) {
+		if (piece == null)
+			throw new IllegalArgumentException();
+		pieceOnSquare = piece;
+	}
+
+	/**
+	 * Get the Piece that is on the Square.
+	 * 
+	 * @return The Piece that is on the Square.
+	 */
+	public Piece getPieceOnSquare() {
+		return pieceOnSquare;
+	}
+
+	/**
+	 *
+	 * @return
+	 */
 	public int getPosX() {
 		return posX;
 	}
+
+	/**
+	 * @param posX
+	 */
 	public void setPosX(int posX) {
 		this.posX = posX;
 	}
+
+	/**
+	 * @return
+	 */
 	public int getPoxY() {
 		return poxY;
 	}
+
+	/**
+	 * @param poxY
+	 */
 	public void setPoxY(int poxY) {
 		this.poxY = poxY;
 	}
+
+	/**
+	 * @return
+	 */
 	public Team getTeam() {
 		return team;
 	}
+
+	/**
+	 * @param team
+	 */
 	public void setTeam(Team team) {
 		if (team == null)
 			throw new IllegalArgumentException("Paramètre null.");
 		this.team = team;
+	}
+
+	@Override
+	public String toString() {
+		return "Square [posX=" + posX + ", poxY=" + poxY + ", team=" + team + "isEmpty=" + this.isEmpty() + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Square other = (Square) obj;
+		if (posX != other.posX)
+			return false;
+		if (poxY != other.poxY)
+			return false;
+		if (team == null) {
+			if (other.team != null)
+				return false;
+		} else if (!team.equals(other.team))
+			return false;
+		return true;
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean isEmpty() {
+		return this.pieceOnSquare == null;
 	}
 
 }
