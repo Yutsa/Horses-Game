bb3ee6655223dbb3c69202187dec0e56e3243b4d
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2016-05-10 17:52:28 +0200

    Finished the initialization of the Board.

--- src/board/BoardHorses.java
+++ src/board/BoardHorses.java
@@ -4,11 +4,14 @@ import game.HorsesGame;
 
 //TODO: Implement BoardHorses.
 public class BoardHorses extends Board {
-	public BoardHorses (HorsesGame game) {
+	public BoardHorses(HorsesGame game) {
 		super(15, 15, game);
 		createBasicSquares();
+		createHorsePen();
+		createBottomStairway();
+		createStairwaySquare();
 	}
-	
+
 	public void createBasicSquares() {
 		Square[][] squares = this.getBoard();
 		for (int i = 0; i < 7; i++) {
@@ -36,7 +39,63 @@ public class BoardHorses extends Board {
 			squares[8][y] = new BasicSquare(8, y, this.getGame().getTeam(3), this);
 		}
 	}
+
+	public void createHorsePen() {
+		Square[][] squares = this.getBoard();
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 6; j++) {
+				squares[i][j] = new HorsePen(i, j, this.getGame().getTeam(0), this);
+			}
+		}
+		for (int i = 9; i < 15; i++) {
+			for (int j = 0; j < 6; j++) {
+				squares[i][j] = new HorsePen(i, j, this.getGame().getTeam(1), this);
+			}
+		}
+		for (int i = 0; i < 6; i++) {
+			for (int j = 8; j < 15; j++) {
+				squares[i][j] = new HorsePen(i, j, this.getGame().getTeam(2), this);
+			}
+		}
+		for (int i = 9; i < 15; i++) {
+			for (int j = 8; j < 15; j++) {
+				squares[i][j] = new HorsePen(i, j, this.getGame().getTeam(3), this);
+			}
+		}
+	}
+
+	public void createBottomStairway() {
+		Square[][] squares = this.getBoard();
+		squares[0][7] = new BottomStairway(0, 7, this.getGame().getTeam(0), this);
+
+		squares[7][0] = new BottomStairway(7, 0, this.getGame().getTeam(1), this);
+
+		squares[14][7] = new BottomStairway(14, 7, this.getGame().getTeam(2), this);
+
+		squares[7][14] = new BottomStairway(7, 14, this.getGame().getTeam(3), this);
+
+	}
 	
+	public void createStairwaySquare() {
+		Square[][] squares = this.getBoard();
+		int j = 1;
+		for (int i = 1; i < 7; i++) {
+			squares[i][7] = new StairwaySquare(i, 7, this.getGame().getTeam(0), this, i);
+		}
+		for (int i = 1; i < 7; i++) {
+			squares[7][i] = new StairwaySquare(7, i, this.getGame().getTeam(1), this, i);
+		}
+		for (int i = 13; i > 7; i--) {
+			squares[i][7] = new StairwaySquare(i, 7, this.getGame().getTeam(2), this, j);
+			j++;
+		}
+		j = 1;
+		for (int i = 13; i > 7; i--) {
+			squares[7][i] = new StairwaySquare(7, i, this.getGame().getTeam(3), this, j);
+			j++;
+		}
+	}
+
 	@Override
 	public String toString() {
 		String board = "";
@@ -45,19 +104,23 @@ public class BoardHorses extends Board {
 			for (int y = 0; y < this.getWidth(); y++) {
 				if (squares[y][x] instanceof BasicSquare) {
 					board += "b";
+				} else if (squares[y][x] instanceof HorsePen) {
+					board += "p";
+				} else if (squares[y][x] instanceof BottomStairway) {
+					board += "o";
+				} else if (squares[y][x] instanceof StairwaySquare){
+					StairwaySquare s = (StairwaySquare) squares[y][x];
+					board += s.getNbStairway();
 				}
 				else {
 					board += "-";
 				}
-				/*else if (squares[i][j] instanceof HorsePen) {
-					board += "p";
-				}
-				else if (squares[i][j] instanceof BottomStairway) {
-					board += "bo";
-				}
-				else if (squares[i][j] instanceof StairwaySquare) {
-					board += "s";
-				}*/
+				/*
+				 * else if (squares[i][j] instanceof HorsePen) { board += "p"; }
+				 * else if (squares[i][j] instanceof BottomStairway) { board +=
+				 * "bo"; } else if () {
+				 * board += "s"; }
+				 */
 			}
 			board += "\n";
 		}
--- src/board/StairwaySquare.java
+++ src/board/StairwaySquare.java
@@ -4,10 +4,21 @@ import team.Team;
 
 //TODO: Implement StairwaySquare.
 public class StairwaySquare extends Square{
-
-	public StairwaySquare(int posX, int posY, Team team, Board board) {
+	private int nbStairway;
+	
+	public StairwaySquare(int posX, int posY, Team team, Board board, int nbStairway) {
 		super(posX, posY, team, board);
 		// TODO Auto-generated constructor stub
+		setNbStairway(nbStairway);
 	}
 
+	public int getNbStairway() {
+		return nbStairway;
+	}
+
+	public void setNbStairway(int nbStairway) {
+		if (nbStairway < 1 || nbStairway > 6)
+			throw new IllegalArgumentException();
+		this.nbStairway = nbStairway;
+	}
 }
